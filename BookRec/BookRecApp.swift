39f0df7c97a3c1eb703e2b50694c211ee24e6a17
celsi <cvanessaa@gmail.com>
2024-05-21 17:06:29 +0700

    Initial Commit

---
+++ BookRec/BookRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRecApp.swift
+//  BookRec
+//
+//  Created by Chelsea Vanessa on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
